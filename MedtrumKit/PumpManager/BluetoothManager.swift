f7694d737209a842772d41326ab6f2bf2908fc5d
Iurii Malchenko <i@yurique.com>
2026-08-18 20:22:52 +0200

    revert simulator bypass

--- MedtrumKit/PumpManager/BluetoothManager.swift
+++ MedtrumKit/PumpManager/BluetoothManager.swift
@@ -477,31 +477,26 @@ extension BluetoothManager {
             return
         }
 
-        let manufacturerData = advertisementData[CBAdvertisementDataManufacturerDataKey] as? Data
-        guard let manufacturerData, manufacturerData.count >= 8 else {
-            #if targetEnvironment(simulator)
-                // Simulator bypass -> 200u
-                scanCompletion?(
-                    .success(
-                        peripheral: peripheral,
-                        pumpSN: Data([0x28, 0xD8, 0x12, 0x4A]),
-                        deviceType: 1,
-                        version: 1
-                    )
+        let manufacturerData = advertisementData[CBAdvertisementDataManufacturerDataKey]
+        guard let manufacturerData = manufacturerData as? Data, manufacturerData.count >= 7 else {
+            // Simulator bypass -> 200u
+            scanCompletion?(
+                .success(
+                    peripheral: peripheral,
+                    pumpSN: Data([0x28, 0xD8, 0x12, 0x4A]),
+                    deviceType: 1,
+                    version: 1
                 )
-                // Simulator bypass -> 300u
-                scanCompletion?(
-                    .success(
-                        peripheral: peripheral,
-                        pumpSN: Data([0x14, 0x16, 0xDF, 0x52]),
-                        deviceType: 1,
-                        version: 1
-                    )
+            )
+            // Simulator bypass -> 300u
+            scanCompletion?(
+                .success(
+                    peripheral: peripheral,
+                    pumpSN: Data([0x14, 0x16, 0xDF, 0x52]),
+                    deviceType: 1,
+                    version: 1
                 )
-            #else
-                // On device this is some other peripheral calling itself MT, not a pump.
-                logger.debug("Ignoring MT advertisement with \(manufacturerData?.count ?? 0) bytes of manufacturer data")
-            #endif
+            )
             return
         }
 
